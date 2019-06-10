35f33a1567d6abecec1c5d3cd31f46e0f19defde
westers209 <jae509@gmail.com>
2019-06-10 12:48:15 -0400

    .

---
+++ app/src/main/java/com/example/magictheguessing/model/PlayData.java
@@ -0,0 +1,13 @@
+package com.example.magictheguessing.model;
+
+public class PlayData {
+    private String userInput;
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+}
